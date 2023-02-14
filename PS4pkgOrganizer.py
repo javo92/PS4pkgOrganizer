03a2716647e9b35464b9adfffdc2393795496790
javo92 <javierbenacaro@gmail.com>
2023-02-15 00:46:33 +0100

    feat: Added verbose and dry_run options

--- PS4pkgOrganizer.py
+++ PS4pkgOrganizer.py
@@ -3,11 +3,37 @@
 
 import os
 import shutil
-
+import sys
+import argparse
 from fnmatch import fnmatch
 
-dry_run = 0
-root = './'
+
+parser = argparse.ArgumentParser(description=\
+  'Process and organize PS4 pkgs using tags of the names in the following the structure.\
+    {PSID} {TYPE} {VERSION} {REGION} GAME_NAME.pkg\
+    example: {CUSA23491} {Game} {01.00} {EU} Bloodstained - Curse of the Moon 2.pkg\
+    By default, it will process recursively, starting at the location of the script.')
+
+parser.add_argument('-p', '--path', dest = 'root',
+                    action = 'store', default = os.path.dirname(os.path.abspath(__file__)),
+                    help = 'Specify the location of the pkgs to organize.')
+
+parser.add_argument('-v', '--verbose', dest = 'verbose',
+                    action = 'store_true', default = False,
+                    help = 'Enable to show relevant information about the organization of the pkgs.')
+parser.add_argument('-dr', '--dry_run', dest = 'dry_run',
+                    action = 'store_true', default = False,
+                    help = 'Do not perform the actions, only show what it would do.')
+
+# Parse arguments and use them as the previously declared variables.
+args = parser.parse_args()
+root = args.root
+dry_run = args.dry_run
+if dry_run == True:
+  verbose = True
+else:
+  verbose = args.verbose
+#root = os.path.dirname(os.path.abspath(__file__))
 pattern = "*.pkg"
 
 # Python3 code here creating class
@@ -27,6 +53,10 @@ class pkg:
       self.type = ""
       self.region = ""
       self.version = ""
+
+def printv (value, verbose = True):
+  if verbose == True:
+    print(value)
 
 def mapParams (game_name):
   # Clear the attributes of the pkg instance to assign new ones
@@ -54,14 +84,14 @@ def mapParams (game_name):
   
   if pkg.region == "":
     pkg.region = "EU"
-    print("pkg region not detected in: " + pkg.name)
+    printv("pkg region not detected in: " + pkg.name)
   if pkg.type == "":
     pkg.type = "Others"
-    print("pkg type not detected in: " + pkg.name)
+    printv("pkg type not detected in: " + pkg.name)
   if pkg.version == "":
     if pkg.type != "Addon":
       pkg.version = "v00.00"
-      print("pkg version not detected in: " + pkg.name)
+      printv("pkg version not detected in: " + pkg.name)
 
 # creating lists
 pkg_Game_list = []
@@ -90,21 +120,28 @@ for path, subdirs, files in os.walk(root):
 def checkListContent(elementlist):
   for element in elementlist:
     if pkg_game.psid == element.psid:
-      if dry_run == 0:
-        if new_game_dir != element.filepath:
-          print(os.path.join(element.filepath, element.filename) + " -> " + new_game_dir)
+      OpStatus = ""
+      if full_game_dir != element.filepath:
+        if dry_run == False:
           shutil.move(os.path.join(element.filepath, element.filename), new_game_dir)
-          # Remove the element from the list to prevent it to be processed again
+      else:
+        OpStatus = (" (File not moved)")
+      # Remove the element from the list to prevent it to be processed again
+      printv(' ├─ ' + os.path.join(element.filepath, element.filename) + OpStatus, verbose)
       elementlist.remove(element)
 
 for pkg_game in pkg_Game_list:
   new_game_dir = pkg_game.name + " [" + pkg_game.region + "]" + " [" + pkg_game.psid + "]"
-  print(os.path.join(pkg_game.filepath, pkg_game.filename) + " -> " + new_game_dir)
-  if dry_run == 0:
-    if new_game_dir != pkg_game.filepath:
-      if not(os.path.exists(new_game_dir)):
-        os.mkdir(new_game_dir)
-        shutil.move(os.path.join(pkg_game.filepath, pkg_game.filename), new_game_dir)
+  full_game_dir = os.path.join(root, pkg_game.name + " [" + pkg_game.region + "]" + " [" + pkg_game.psid + "]")
+  OpStatus = ""
+  if full_game_dir != pkg_game.filepath:
+    if dry_run == False:
+      if not(os.path.exists(full_game_dir)):
+        os.mkdir(full_game_dir)
+        shutil.move(os.path.join(pkg_game.filepath, pkg_game.filename), full_game_dir)
+  else:
+    OpStatus = (" (File not moved)")
+  printv(os.path.join(pkg_game.filepath, pkg_game.filename) + " -> \n" + full_game_dir + OpStatus, verbose)
   # Check the Patches
   checkListContent(pkg_Patch_list)
   # Check the Addons
